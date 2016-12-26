a97cfabb222bc7836f5b5faa5f2c1f10837a0cb6
Alex Navasardyan <anavasardyan@linkedin.com>
2016-12-25 16:36:10 -0800

    SAVEPOINT

--- lib/actor.ts
+++ lib/actor.ts
@@ -1,20 +1,40 @@
 import Message from './message';
 import uuid from 'node-uuid';
+import { defer } from 'rsvp';
+
 /* eslint-disable */
 import regeneratorRuntime from 'regenerator-runtime';
 /* eslint-enable */
 
 export default class Actor {
+  id: string;
+  state: Promise<any>;
+
   constructor(system) {
     //if (!system) {
     //  throw new Error('Creating actor using `new` is not allowed.');
     //}
 
-    this.parent = system;
+    this.system = system;
     this.id = uuid.v4();
+    this.state = defer();
   }
 
-  send() {}
+  preStart() {}
+
+  postStart() {}
+
+  setResolved(state: any) {
+    this.state.resolve(state);
+  }
+
+  setRejected(state: any) {
+    this.state.reject(state);
+  }
+
+  send(message: Message) {
+    this.system.send(message);
+  }
 
   //async send(message) {
   //  let messageInstance = new Message({
